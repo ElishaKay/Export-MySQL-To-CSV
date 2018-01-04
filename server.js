1a5079225e0df05c83f629539a354d7ddb9edd77
elishakay <kramer1346@gmail.com>
2018-01-04 23:19:37 +0200

    easily exports over 4,000 rows

--- server.js
+++ server.js
@@ -22,8 +22,9 @@ app.get('/',function(req,res){
 
 app.get('/api/mysql',function(req,res){
         var row = [];
-        connection.query('select * from client', function (err, rows) {
-            res.json(rows);
+        connection.query("select IFNULL(r.receiver_first_name,'') as 'First Name' , IFNULL(r.receiver_last_name,'') as 'Last Name', IFNULL(r.receiver_headline,'') as 'Headline', IFNULL(r.receiver_username,'') as 'Username', IFNULL(r.receiver_profile_id,'') as 'SN Profile_id', IFNULL(r.receiver_email,'') as 'Email', IFNULL(r.receiver_phone,'') as 'Phone', IFNULL(r.receiver_location,'') as 'Location', IFNULL(r.receiver_linkedin_url,'') as 'LinkedIn Url', IFNULL(r.receiver_twitter_url,'') as 'Twitter Url', IFNULL(r.receiver_websites,'') as 'Websites', IFNULL(r.receiver_ims,'') as 'Ims', IFNULL(r.receiver_birthday,'') as 'Birthday', IFNULL(ur.user_receiver_creation_date,'') as 'Connection Date' from user_receiver ur inner join user u on (u.user_id = ur.user_id) inner join client cl on (cl.client_id = u.client_id) inner join receiver r on (r.receiver_id=ur.receiver_id) where cl.client_id=402", 
+			function (err, rows) {
+				res.json(rows);
         });      
     });
 
